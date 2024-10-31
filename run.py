447e3b95732e01fb823d6da7b493d65c4e64c267
Radmehr <radmehrvafadar@gmail.com>
2024-10-31 18:55:56 -0400

    Indentation bug Fix

--- run.py
+++ run.py
@@ -52,39 +52,38 @@ class FlowFree:
         return connections
 
     def encode(self):
-def encode(self):
-    # Add constraints here
-    # 1. Each cell must be used exactly once
-    for i in range(self.size):
-        for j in range(self.size):
-            self.E.add_constraint(ExactlyOne([conn for conn in self.connections if conn.x1 == i and conn.y1 == j or conn.x2 == i and conn.y2 == j]))
-
-    # 2. Each colored endpoint must connect to exactly one neighbor
-    for color in self.colors:
+        # Add constraints here
+        # 1. Each cell must be used exactly once
         for i in range(self.size):
             for j in range(self.size):
-                if self.grid[i][j] == color:
-                    self.E.add_constraint(ExactlyOne([conn for conn in self.connections if (conn.x1 == i and conn.y1 == j) or (conn.x2 == i and conn.y2 == j)]))
+                self.E.add_constraint(ExactlyOne([conn for conn in self.connections if conn.x1 == i and conn.y1 == j or conn.x2 == i and conn.y2 == j]))
 
-    # 3. Each intermediate cell must connect to exactly two neighbors
-    for i in range(self.size):
-        for j in range(self.size):
-            if self.grid[i][j] == '.':
-                self.E.add_constraint(ExactlyTwo([conn for conn in self.connections if (conn.x1 == i and conn.y1 == j) or (conn.x2 == i and conn.y2 == j)]))
+        # 2. Each colored endpoint must connect to exactly one neighbor
+        for color in self.colors:
+            for i in range(self.size):
+                for j in range(self.size):
+                    if self.grid[i][j] == color:
+                        self.E.add_constraint(ExactlyOne([conn for conn in self.connections if (conn.x1 == i and conn.y1 == j) or (conn.x2 == i and conn.y2 == j)]))
 
-    # 4. Paths must be continuous
-    for conn in self.connections:
-        self.E.add_constraint(Implies(conn.active, And(
-            Or([other_conn.active for other_conn in self.connections if (other_conn.x1 == conn.x2 and other_conn.y1 == conn.y2) or (other_conn.x2 == conn.x2 and other_conn.y2 == conn.y2)]),
-            Or([other_conn.active for other_conn in self.connections if (other_conn.x1 == conn.x1 and other_conn.y1 == conn.y1) or (other_conn.x2 == conn.x1 and other_conn.y2 == conn.y1)])
-        )))
+        # 3. Each intermediate cell must connect to exactly two neighbors
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.grid[i][j] == '.':
+                    self.E.add_constraint(ExactlyTwo([conn for conn in self.connections if (conn.x1 == i and conn.y1 == j) or (conn.x2 == i and conn.y2 == j)]))
 
-    # 5. Paths cannot cross
-    for conn1 in self.connections:
-        for conn2 in self.connections:
-            if conn1 != conn2:
-                self.E.add_constraint(Not(And(conn1.active, conn2.active, conn1.x1 == conn2.x1, conn1.y1 == conn2.y1, conn1.x2 == conn2.x2, conn1.y2 == conn2.y2)))
-        pass
+        # 4. Paths must be continuous
+        for conn in self.connections:
+            self.E.add_constraint(Implies(conn.active, And(
+                Or([other_conn.active for other_conn in self.connections if (other_conn.x1 == conn.x2 and other_conn.y1 == conn.y2) or (other_conn.x2 == conn.x2 and other_conn.y2 == conn.y2)]),
+                Or([other_conn.active for other_conn in self.connections if (other_conn.x1 == conn.x1 and other_conn.y1 == conn.y1) or (other_conn.x2 == conn.x1 and other_conn.y2 == conn.y1)])
+            )))
+
+        # 5. Paths cannot cross
+        for conn1 in self.connections:
+            for conn2 in self.connections:
+                if conn1 != conn2:
+                    self.E.add_constraint(Not(And(conn1.active, conn2.active, conn1.x1 == conn2.x1, conn1.y1 == conn2.y1, conn1.x2 == conn2.x2, conn1.y2 == conn2.y2)))
+            pass
 
     def solve(self):
         theory = self.E.compile()
