23b45a0e163a147accdd1aeb90b09fbe8ef85f76
Radmehr <23xf1@queensu.ca>
2024-10-31 21:19:19 -0400

    finilised draft

--- flow_free.py
+++ flow_free.py
@@ -1,30 +1,42 @@
-from bauhaus import Encoding, proposition, constraint, And, Or
+import pprint
+
+from bauhaus import Encoding, proposition, constraint
 from bauhaus.utils import count_solutions, likelihood
 from nnf import config
 config.sat_backend = "kissat"
 
-# Create global encoding to be used
+# Create global encoding
 E = Encoding()
 
 @proposition(E)
-class CellConnection:
-    def __init__(self, x1, y1, x2, y2, color):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
+class Cell(object):
+    def __init__(self, x, y, color=None):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def _prop_name(self):
+        return f"Cell({x},{y},{color})"
+
+
+@proposition(E)
+class Connection(object):
+    def __init__(self, cell1, cell2, color):
+        self.cell1 = cell1
+        self.cell2 = cell2
         self.color = color
     
-    def __repr__(self):
-        return f"{self.color}: ({self.x1},{self.y1}) -> ({self.x2},{self.y2})"
+    def _prop_name(self):
+        return f"Connection({cell1}->{cell2}, {color})"
 
 class FlowFree:
     def __init__(self, filename):
-        self.E = E  # Use the global encoding
+        self.encoding = E
         self.grid, self.colors, self.size = self._read_input(filename)
-        self.connections = self._create_propositions()
+        self.cells = self._create_cells()
+        self.connections = self._create_connections()
         self.color_endpoints = self._find_endpoints()
-    
+
     def _read_input(self, filename):
         grid = []
         colors = set()
@@ -38,7 +50,36 @@ class FlowFree:
                     colors.add(cell)
         
         return grid, colors, len(grid)
-    
+
+    def _create_cells(self):
+        cells = {}
+        for i in range(self.size):
+            for j in range(self.size):
+                color = self.grid[i][j] if self.grid[i][j] != '.' else None
+                cells[(i,j)] = Cell(i, j, color)
+        return cells
+
+    def _create_connections(self):
+        connections = []
+        for i in range(self.size):
+            for j in range(self.size):
+                # Check adjacent cells (right and down only to avoid duplicates)
+                if j < self.size - 1:  # right
+                    for color in self.colors:
+                        connections.append(Connection(
+                            self.cells[(i,j)],
+                            self.cells[(i,j+1)],
+                            color
+                        ))
+                if i < self.size - 1:  # down
+                    for color in self.colors:
+                        connections.append(Connection(
+                            self.cells[(i,j)],
+                            self.cells[(i+1,j)],
+                            color
+                        ))
+        return connections
+
     def _find_endpoints(self):
         endpoints = {}
         for color in self.colors:
@@ -46,97 +87,25 @@ class FlowFree:
             for i in range(self.size):
                 for j in range(self.size):
                     if self.grid[i][j] == color:
-                        endpoints[color].append((i, j))
+                        endpoints[color].append(self.cells[(i,j)])
         return endpoints
-
-    def _create_propositions(self):
-        connections = []
-        # Create possible connections between adjacent cells
-        for i in range(self.size):
-            for j in range(self.size):
-                # Horizontal connections
-                if j < self.size - 1:
-                    for color in self.colors:
-                        connections.append(CellConnection(i, j, i, j+1, color))
-                        connections.append(CellConnection(i, j+1, i, j, color))
-                # Vertical connections
-                if i < self.size - 1:
-                    for color in self.colors:
-                        connections.append(CellConnection(i, j, i+1, j, color))
-                        connections.append(CellConnection(i+1, j, i, j, color))
-        return connections
-
-    def _get_connections_for_cell(self, x, y):
-        return [conn for conn in self.connections 
-                if (conn.x1 == x and conn.y1 == y) or (conn.x2 == x and conn.y2 == y)]
 
     def encode(self):
         # 1. Each cell must be used exactly once
-        for i in range(self.size):
-            for j in range(self.size):
-                cell_connections = self._get_connections_for_cell(i, j)
-                constraint.add_exactly_one(self.E, cell_connections)
 
         # 2. Colored endpoints must connect to exactly one neighbor
-        for color, endpoints in self.color_endpoints.items():
-            for x, y in endpoints:
-                # Get all possible connections for this endpoint of this color
-                endpoint_connections = [conn for conn in self._get_connections_for_cell(x, y)
-                                     if conn.color == color]
-                constraint.add_exactly_one(self.E, endpoint_connections)
-                
-                # Forbid connections of other colors
-                other_color_connections = [conn for conn in self._get_connections_for_cell(x, y)
-                                        if conn.color != color]
-                for conn in other_color_connections:
-                    self.E.add_constraint(~conn)
 
         # 3. Non-endpoint cells must connect to exactly two neighbors
-        for i in range(self.size):
-            for j in range(self.size):
-                if self.grid[i][j] == '.':  # Non-endpoint cell
-                    cell_connections = self._get_connections_for_cell(i, j)
-                    # Group connections by color
-                    for color in self.colors:
-                        color_connections = [conn for conn in cell_connections 
-                                          if conn.color == color]
-                        # If this cell is used by this color, it must have exactly two connections
-                        if color_connections:
-                            constraint.add_exactly_k(self.E, color_connections, 2)
 
         # 4. Paths must be continuous
-        for color in self.colors:
-            start, end = self.color_endpoints[color]
-            # For each intermediate cell
-            for i in range(self.size):
-                for j in range(self.size):
-                    if (i,j) != start and (i,j) != end:
-                        incoming = [conn for conn in self.connections 
-                                  if conn.color == color and (conn.x2,conn.y2) == (i,j)]
-                        outgoing = [conn for conn in self.connections 
-                                  if conn.color == color and (conn.x1,conn.y1) == (i,j)]
-                        
-                        # If there's an incoming connection, there must be an outgoing one
-                        for inc in incoming:
-                            self.E.add_constraint(inc >> Or(outgoing))
 
         # 5. Paths cannot cross
-        for i in range(self.size):
-            for j in range(self.size):
-                # For each cell, ensure only one color can use it
-                for color1 in self.colors:
-                    for color2 in self.colors:
-                        if color1 < color2:  # Avoid duplicate constraints
-                            color1_conns = [conn for conn in self._get_connections_for_cell(i, j)
-                                          if conn.color == color1]
-                            color2_conns = [conn for conn in self._get_connections_for_cell(i, j)
-                                          if conn.color == color2]
-                            for c1 in color1_conns:
-                                for c2 in color2_conns:
-                                    self.E.add_constraint(~(c1 & c2))
+
+        # 7. Grid must be n x n
+        pass
 
     def solve(self):
-        theory = self.E.compile()
+        theory = self.encoding.compile()
         solution = theory.solve()
         if solution:
             return self._extract_paths(solution)
@@ -149,5 +118,7 @@ class FlowFree:
                 color = conn.color
                 if color not in paths:
                     paths[color] = []
-                paths[color].append((conn.x1, conn.y1, conn.x2, conn.y2))
+                paths[color].append(
+                    (conn.cell1.x, conn.cell1.y, conn.cell2.x, conn.cell2.y)
+                )
         return paths
